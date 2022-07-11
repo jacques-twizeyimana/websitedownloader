f26a050eb1e780c47de4fd45f2212a0088af8cb3
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-11 12:19:26 +0200

    feat: services

--- src/main/java/ac/rca/downloaderbackend/services/ILinkService.java
+++ src/main/java/ac/rca/downloaderbackend/services/ILinkService.java
@@ -1,2 +1,26 @@
-package ac.rca.downloaderbackend.services;public interface ILinkService {
+package ac.rca.downloaderbackend.services;
+
+import ac.rca.downloaderbackend.model.Links;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import java.util.List;
+import java.util.UUID;
+
+
+public interface ILinkService {
+    public List<Links> getAll();
+
+    public Page<Links> getAll(Pageable pageable);
+
+    public Links getById(UUID id);
+
+    public Links create(Links Links);
+
+    public Links update(UUID id, Links Links);
+
+    public boolean delete(UUID id);
+
+    public List<Links> searchLinks(String searchKey);
 }
--- src/main/java/ac/rca/downloaderbackend/services/IWebsiteService.java
+++ src/main/java/ac/rca/downloaderbackend/services/IWebsiteService.java
@@ -1,2 +1,26 @@
-package ac.rca.downloaderbackend.services;public interface IWebsiteService {
+package ac.rca.downloaderbackend.services;
+
+import ac.rca.downloaderbackend.model.Website;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import java.util.List;
+import java.util.UUID;
+
+
+public interface IWebsiteService {
+    public List<Website> getAll();
+
+    public Page<Website> getAll(Pageable pageable);
+
+    public Website getById(UUID id);
+
+    public Website create(Website Website);
+
+    public Website update(UUID id, Website Website);
+
+    public boolean delete(UUID id);
+
+    public List<Website> searchWebsite(String searchKey);
 }

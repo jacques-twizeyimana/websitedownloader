287d79c749019409681f0562dd19b87d98a40208
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-11 12:13:05 +0200

    feat: database models

---
+++ src/main/java/ac/rca/downloaderbackend/model/Website.java
@@ -0,0 +1,95 @@
+package ac.rca.downloaderbackend.model;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Entity
+@Table(name="website")
+public class Website {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private UUID id;
+
+    @Column(name="website_name")
+    private String websiteName;
+
+    @Column(name="download_start_date_time")
+    private String downloadStartTime;
+
+
+    @Column(name="download_end_date_time")
+    private String downloadEndTime;
+
+    @Column(name="total_elapsed_time")
+    private String elapsedTime;
+
+    @Column(name="total_download_kilobytes")
+    private String totalKilobytes;
+
+    @OneToMany
+    @JoinColumn(name = "website_id")
+    ArrayList<Links> links;
+
+    public Website() {
+    }
+
+    public Website(String websiteName, String downloadStartTime, String downloadEndTime, String elapsedTime, String totalKilobytes) {
+        this.websiteName = websiteName;
+        this.downloadStartTime = downloadStartTime;
+        this.downloadEndTime = downloadEndTime;
+        this.elapsedTime = elapsedTime;
+        this.totalKilobytes = totalKilobytes;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getWebsiteName() {
+        return websiteName;
+    }
+
+    public void setWebsiteName(String websiteName) {
+        this.websiteName = websiteName;
+    }
+
+    public String getDownloadStartTime() {
+        return downloadStartTime;
+    }
+
+    public void setDownloadStartTime(String downloadStartTime) {
+        this.downloadStartTime = downloadStartTime;
+    }
+
+    public String getDownloadEndTime() {
+        return downloadEndTime;
+    }
+
+    public void setDownloadEndTime(String downloadEndTime) {
+        this.downloadEndTime = downloadEndTime;
+    }
+
+    public String getElapsedTime() {
+        return elapsedTime;
+    }
+
+    public void setElapsedTime(String elapsedTime) {
+        this.elapsedTime = elapsedTime;
+    }
+
+    public String getTotalKilobytes() {
+        return totalKilobytes;
+    }
+
+    public void setTotalKilobytes(String totalKilobytes) {
+        this.totalKilobytes = totalKilobytes;
+    }
+}

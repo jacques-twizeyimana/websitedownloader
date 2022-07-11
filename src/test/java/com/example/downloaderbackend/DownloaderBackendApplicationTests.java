2231e6930e034497c5a907d17f0c91c38ccd4d55
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-11 11:47:00 +0200

    initial

---
+++ src/test/java/com/example/downloaderbackend/DownloaderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.downloaderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
